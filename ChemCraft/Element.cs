dda2244d278cda8991dec5c7d7c2ea2c29c640e5
Tojnagag <1bhullargag@hdsb.ca>
2015-06-02 21:37:06 -0400

    Update Element.cs

--- ChemCraft/Element.cs
+++ ChemCraft/Element.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChemCraft
+{
+    abstract partial class Element
+    {
+        //transmute the element up or down a level on the periodic table
+        abstract Element transmute;
+
+        //attack with the element, for a simple attack
+        abstract void attack;
+
+        //defend with the element, for a simple shield
+        abstract void defense;
+
+        //amount of electrons, ionic charge of the element, and how reactive the element is
+        public int electrons, reactivity, state = 0;
+
+
+    }
+}
 
