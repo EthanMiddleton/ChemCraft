9eecad8d5baf387f2f727e58d022d3e418de747a
Sfmwzo <jasdip_sfmwzo@hotmail.com>
2015-06-05 11:06:14 -0400

    Update Compound Abstract.cs

--- ChemCraft/Compound Abstract.cs
+++ ChemCraft/Compound Abstract.cs
@@ -1,41 +1,60 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Chemcraft
 {
-    abstract class AbstractCompounds
+     public abstract class Compounds
     {
         
         //number of elements
-        abstract private int elementnum;
+        private int elementnum;
         // whether the attacking is true
-        abstract private int state;
+        private int state;
         //damage numbers
-        abstract private int damage;
+        private int damage;
         //name of compound
-        abstract private string name;
+        private string name;
         //array of elements
-        abstract private string[] elements;
+        private enum elements { };
         //chemical formula
-        abstract private string formula;
-        //acid or base
-        abstract private int acidorbase;
-
+        private string formula;
+        //type of element
+        private enum type{};
+        //arraylist of elements
+        private ArrayList elements;
 
         /// <summary>
         /// determines if the compound attacks that turn
         /// </summary>
         /// <returns>if compund attacks returns true else false</returns>
-        abstract private bool attack();
+        abstract private int attack();
 
         /// <summary>
         /// determines if the compund defends on that turn
         /// </summary>
         /// <returns>if the compund defends return true else false</returns>
-        abstract private bool defend();
-       
-        
+        abstract private int defend();
+
+        /// <summary>
+        /// gets the name of compund
+        /// </summary>
+        /// <returns>name</returns>
+        abstract private string GetName();
+
+        /// <summary>
+        /// gets chemical formula
+        /// </summary>
+        /// <returns>formula</returns>
+        abstract public string GetFormula();
+
+        /// <summary>
+        ///the compounds ability
+        /// </summary>
+        abstract public void ability();
+
+
     }
 }
