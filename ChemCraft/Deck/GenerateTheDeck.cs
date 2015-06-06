bd15d7177b93c3cefc7ae78a405de95431aa24e1
Omer Uddin <kickash32@gmail.com>
2015-06-05 22:56:41 -0400

    Update and rename Class2.cs to GenerateTheDeck.cs

--- ChemCraft/Deck/GenerateTheDeck.cs
+++ ChemCraft/Deck/GenerateTheDeck.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class GenerateTheDeck
+    public class GenerateTheDeck
     {
         public static void main(string[] args)
         {
