7bea43e770d49e09473f11f46d4e5f5acd152c16
Tojnagag <1bhullargag@hdsb.ca>
2015-06-11 11:53:29 -0400

    Update Rubbing-Alcohol.cs

--- ChemCraft/Compounds/Rubbing-Alcohol.cs
+++ ChemCraft/Compounds/Rubbing-Alcohol.cs
@@ -9,13 +9,14 @@ namespace ChemCraft
     public class C3H8O : Compound
     {
 
-        public C3H8O() {
+        public C3H8O(int[] myIDs) {
             name = "Rubbing Alcohol";
             elementnum = 3;
             damage = 2;
             formula = "C₃H₈O";
             type = Type.Undetermined;
             elements = new int[12] {6,6,6,1,1,1,1,1,1,1,1,8};
+            IDS = myIDs;
         }
 
    
